4a4205f07f4374af8bf24c28a89252f98a667623
uabryanblue <bryanblue@arizona.edu>
2022-11-24 13:39:51 +0000

    Fixed column name issue and loading of stations.csv file

--- FinalDataProject.R
+++ FinalDataProject.R
@@ -24,7 +24,7 @@ AZMET_Filenames <- list.files(path = here("data"), full.names = TRUE, pattern = 
 
 ## Read in the CSV files and assign column names.
 AZMET <- read_csv(file = AZMET_Filenames, 
-                  col_names=c("Year","DOY","SatationNumber","AirTMax","AirTMin","AirTMean","RHMax","RHMin","RHMean","VPDMean","SolarRadTotal","PrecipTotal","SoilTempMax_4in","SoilTempMin_4in","SoilTempMean_4in","SoilTempMax_20in","SoilTempMin_20in","SoilTempMean_20in","WindSpeedMean","WindVecMag_day","WindVecDir_day","WindDirSTD_day","WindSpeedMax","HeatUnits","ETo","RefETo","ActuaVPMean_day","DewpoinMean_day"),
+                  col_names=c("Year","DOY","StationNumber","AirTMax","AirTMin","AirTMean","RHMax","RHMin","RHMean","VPDMean","SolarRadTotal","PrecipTotal","SoilTempMax_4in","SoilTempMin_4in","SoilTempMean_4in","SoilTempMax_20in","SoilTempMin_20in","SoilTempMean_20in","WindSpeedMean","WindVecMag_day","WindVecDir_day","WindDirSTD_day","WindSpeedMax","HeatUnits","ETo","RefETo","ActuaVPMean_day","DewpoinMean_day"),
                   show_col_types = FALSE)
 
 ## Create a new column called DATE that is an actual date in R
@@ -37,7 +37,19 @@ AZMET$DATE <- as.Date(AZMET$DOY-1, origin = paste(AZMET$Year, "-01-01", sep = ""
 # file will make sense on its own
 write.csv(AZMET, "AZMET.csv", row.names = TRUE)
 
+## load station id, name table as a cross-reference
+# specify column names that match other data
+Stations <- read_csv(file = here("stations.csv"), 
+         col_names=c("StationNumber","StationName"),
+         skip = 1,
+         show_col_types = FALSE)
+
 ##### Begin Your Project Code Here #####
+
+# TODO: PUT THE STATION NAME IN THE AZMET DATA
+# Use "left_join( )" on AZMET with Stations dataframe using
+# the key called "StationNumber" that appears in both
+
 
 head(AZMET)
 AZMET_Filenames
